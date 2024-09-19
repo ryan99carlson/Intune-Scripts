04d85438ae97fc039081a4a6069ebf4c76dbc8c4
Ryan Carlson <ryan.carlson@cqmedical.com>
2024-09-19 13:00:24 -0400

    updated script to install package provider NuGet

--- Win10_InstallWinUpdates.ps1
+++ Win10_InstallWinUpdates.ps1
@@ -1,4 +1,8 @@
+#Install Nuget Package Provider
+    Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+
 #Install PSWindows update Module
     Install-Module PSWindowsUpdate -Force
+    
 #Get & Install windows updates
     Install-WindowsUpdate -Confirm:$false -IgnoreReboot
